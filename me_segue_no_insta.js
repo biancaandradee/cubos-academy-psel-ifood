64c1800393d6ee7390ea05fb36e317df6fa02a24
Bianca Andrade <bi_andradehandan@hotmail.com>
2021-06-08 20:17:20 -0300

    Me Segue no Insta!

---
+++ me_segue_no_insta.js
@@ -0,0 +1,9 @@
+function solucao(largura, altura) {
+    if (largura < altura){
+       console.log("RETRATO");
+  } else if (largura > altura){
+       console.log("PAISAGEM");
+  } else if (largura === altura){
+       console.log("QUADRADA");
+  }
+}
